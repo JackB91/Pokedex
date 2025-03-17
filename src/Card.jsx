09b6c7ef0ebc11c7ffc35358b6efd093d2d92d65
JackB91 <you@example.com>
2025-03-16 18:38:11 -0700

    calling the pokemon api

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
 export default function Card() {
+
+    const [pokemonData, setPokemonData] = React.useState({})
+    let min = 1;
+    let max = 151;
+
+    console.log(pokemonData)
+
+    React.useEffect(() => {
+        let num = Math.random() * (max - min) + min;
+        let random = Math.floor(num);
+        fetch(`https://pokeapi.co/api/v2/pokemon/${random}/`)
+        .then (res => res.json())
+        .then(data => setPokemonData(data))
+    },[])
+
+
     return (
         <main>
             <h1>It works</h1>
         </main>
-    )   
+    )  
 }
+
 
