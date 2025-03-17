dfbbfa6fc3feb2a491849dc79c8a3fb3a1e1d6cf
JackB91 <you@example.com>
2025-03-16 18:28:12 -0700

    create root, correct imports

---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <main>
+            <h1>It works</h1>
+        </main>
+    )   
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Card from './Card'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <Card/>
   </StrictMode>,
 )
