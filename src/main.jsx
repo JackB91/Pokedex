c993edad1162526f38f0a42e44a777a777f8e4f0
JackB91 <you@example.com>
2025-03-18 21:34:03 -0700

    Added Header and Footer Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Card from './Card'
+import Card from './components/Card'
+import Header from './components/Header'
+import Footer from './components/footer'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header/>
     <Card/>
+    <Footer/>
   </StrictMode>,
 )
