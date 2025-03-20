bef6c5161e3b8df6d1e0297fb6eb0e4ee7a84284
JackB91 <you@example.com>
2025-03-19 17:15:54 -0700

    restructuring

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from './components/Card'
+import Header from './components/Header'
+import Footer from './components/footer'
+
+
+
+export default function App() {
+
+return (
+    <main>
+            <Header/>
+            <Card/>
+            <Footer/>
+    </main>
+)
+
+}
