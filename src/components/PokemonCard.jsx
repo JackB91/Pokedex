1f2b10abf65d48ca907e9dced945bc37b9418c36
JackB91 <you@example.com>
2025-03-25 06:21:57 -0700

    Some card info

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,4 +3,34 @@ import React from "react";
 
 export default function PokemonCard ({pokemon}) {
 console.log(pokemon.name)
+
+
+return (
+    <div className="pokemon-card">
+        <img  
+            src={pokemon?.sprites?.other?.['official-artwork']?.front_default}
+            alt={pokemon.name} />
+
+        <h2>{pokemon.name}</h2>
+        {/* <p>Height: {pokemon.height/10}m</p>
+        <p>Weight: {pokemon.weight/10}kg</p> */}
+
+        <h3>Base Stats</h3>
+
+        <ul>
+            {pokemon.stats.map((s) => (
+                <li key={s.stat.name}>
+                    {s.stat.name.toUpperCase()}: {s.base_stat} 
+                </li>)
+            )
+            }
+        </ul>
+
+
+
+        <p>Type: {pokemon.types.map(t => t.type.name).join(", ")}</p>
+        {/* <p>Moves: {pokemon.moves.slice(0, 5).map(m => m.move.name).join(", ")}</p> */}
+        
+    </div>
+)
 }
